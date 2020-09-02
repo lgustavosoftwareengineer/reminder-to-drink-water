364d9bb55634ec8ffbb0acc41020ee0f6b0e3f38
Luiz Gustavo <gugamatias7@gmail.com>
2020-09-02 17:14:05 -0300

    ❕Fixing a eslint error

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,49 +1,5 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  h1 {
-    font-size: 1.6rem;
-    color: ${props => props.theme.colors.primary};
-    margin: 30px;
-    text-align: center;
-  }
-
-  .button {
-    display: flex;
-    color: #fff;
-    background: rgba(29, 26, 26, 0);
-    padding: 0.5rem;
-    font-size: 1.2rem;
-    margin-top: 1rem;
-    min-width: 12rem;
-    border: 2px solid #fff;
-    border-radius: 10px;
-    outline-color: #fff;
-    border-style: solid;
-    text-decoration-line: none;
-    justify-content: center;
-    margin: 0 1rem;
-  }
-
-  .button:focus {
-    outline: none;
-  }
-
-  .button:hover {
-    cursor: pointer;
-    color: ${props => props.theme.colors.background};
-    background: ${props => props.theme.colors.primary};
-    transition: all 0.8s ease;
-    border-radius: 20px;
-  }
-`
 export const Button = styled.a`
   p {
     display: flex;
@@ -74,6 +30,21 @@ export const Button = styled.a`
     background: ${props => props.theme.colors.primary};
     transition: all 0.8s ease;
     border-radius: 20px;
+  }
+`
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    font-size: 1.6rem;
+    color: ${props => props.theme.colors.primary};
+    margin: 30px;
+    text-align: center;
   }
 
   @media (min-width: 600px) {

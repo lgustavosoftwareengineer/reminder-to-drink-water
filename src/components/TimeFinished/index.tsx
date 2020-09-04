bcc14f0d83255ccee6c4d280af7a42388a3db422
Luiz Gustavo <gugamatias7@gmail.com>
2020-09-04 13:05:20 -0300

    Change the favicon from the component TimeFinished

--- src/components/TimeFinished/index.tsx
+++ src/components/TimeFinished/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 
 import ReactHowler from 'react-howler'
-import waterGlassFavicon from '../../assets/favicons/water-glass.ico'
+import waveFavicon from '../../assets/favicons/wave.ico'
 
 interface TimeFinished {
   time: number
@@ -15,9 +15,10 @@ const TimeFinished: React.FC<TimeFinished> = ({ time, isPlaying }) => {
       {time <= 0 && (
         <div>
           <Head>
-            <link rel="shortcut icon" href={waterGlassFavicon} />
+            <link rel="shortcut icon" href={waveFavicon} />
+            <title>Água na sua garganta agora! 🌊</title>
           </Head>
-          <h1>Água na sua garganta agora! 🚰</h1>
+          <h1>Água na sua garganta agora! 🌊</h1>
           <ReactHowler
             src="http://soundbible.com/mp3/pour_glass_water-Mike-Koenig-123.mp3"
             playing={isPlaying}

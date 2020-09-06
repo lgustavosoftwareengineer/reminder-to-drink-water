9962cea8892ca11b143c919384be6dc2d9edafc4
Luiz Gustavo <gugamatias7@gmail.com>
2020-09-06 10:53:10 -0300

    Creating the default and light theme

---
+++ src/styles/themes/defaultTheme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#0853c4',
+    text: '#1dafdb',
+    primary: '#d5dfed'
+  }
+}
+
+export default theme
---
+++ src/styles/themes/lightTheme.ts
@@ -0,0 +1,9 @@
+const lightTheme = {
+  colors: {
+    background: '#d5dfed',
+    text: '#1dafdb',
+    primary: '#0853c4'
+  }
+}
+
+export default lightTheme
